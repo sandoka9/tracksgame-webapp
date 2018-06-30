a9c1d13f4cef33ac09cd100673b6324d9aec3107
sandoka9 <chiara123456879@gmail.com>
2018-06-30 12:50:13 +0200

    Add Step 3-4-5

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,12 @@
 /* eslint-disable */
 <template >
   <div class="dropdown" v-bind:class="{ open: activator }" id="bar-nav" v-show="showNavBar">
-   <button class="hamburger btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded=true  @click="makeBurger">
-   &#9776;
+   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded=true  @click="makeBurger">
+   <!-- &#9776; -->
+   <i class="fa fa-th"></i>
    <span class="caret"></span>
    </button>
-   <ul class="dropdown-menu" aria-labelledby="dropdownMenu1" v-bind:style="{ display: isDisplay }" >
+   <ul class="dropdown-menu-sur" ref="liste" aria-labelledby="dropdownMenu1" ><!--v-bind:style="{ display: isDisplay }"-->
      <li @click="makeBurger"><router-link :to="{ name: 'Login' }">Login</router-link></li>
      <li role="separator" class="divider"></li>
      <li @click="makeBurger"><router-link :to="{ name: 'Steps' }">Steps</router-link></li>
@@ -26,6 +27,7 @@ export default {
     }
   },
   mounted: function () {
+    this.$refs['liste'].style.display = 'none'
     if (this.$route.name === 'Login') {
       this.showNavBar = false
     } else {
@@ -35,12 +37,15 @@ export default {
   methods: {
     makeBurger () {
       this.activator = !this.activator
-      if (this.isDisplay === '') {
-        this.isDisplay = 'none'
+      this.check = this.$refs['liste'].style.display
+      if (this.check === '') {
+      /*  this.isDisplay = 'none' */
+        this.$refs['liste'].style.display = 'none'
       } else {
-        this.isDisplay = ''
+      /*  this.isDisplay = '' */
+        this.$refs['liste'].style.display = ''
       }
-      return this.activator
+      return this.$refs['liste'].style.display
     }
   }
 }
@@ -53,14 +58,31 @@ export default {
   overflow: hidden;
   background-color: #e5e5e5;
 }
-.dropdown-menu {
-  font-size: 1.5em;
-  min-width: 100vh;
+.dropdown-menu-sur {
+    position: absolute;
+    top: 100%;
+    left: 0;
+    z-index: 1000;
+    float: left;
+    min-width: 150rem;
+    padding: 0.5rem 0;
+    margin: 0.125rem 0 0;
+    font-size: 3rem;
+    color: #212529;
+    text-align: left;
+    list-style: none;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid rgba(0, 0, 0, 0.15);
+    border-radius: 0.25rem;
 }
 .dropdown {
   font-size: 2rem;
   /*min-width: 50rem;*/
   /*opacity: 0.5;*/
+}
+.divider {
+  border: 1px solid rgba(0, 0, 0, 0.15);
 }
 .btn {
   font-size: 3rem;
--- src/components/Steps.vue
+++ src/components/Steps.vue
@@ -1,30 +1,40 @@
 /* eslint-disable */
 <template>
   <div id="steps">
+    <h1>{{stepIndex}}</h1>
      <h2>{{title[stepIndex]}}</h2>
     <div class="description-bloc">
       <div class="short-description">{{shortDescription[stepIndex]}}</div>
+      <div class="error-msg" v-if="error == true" v-on:click="close">
+        {{errorMsg}}
+      </div>
+      <div class="win-msg" v-if="stepIndex == 4" v-on:click="close">
+        <span>{{winMsg[stepIndex]}}</span>
+        <span>{{indice[stepIndex]}}</span>
+      </div>
+
       <div class="description">{{description[stepIndex]}}</div>
       <div class="info" v-if="stepIndex == 0 || stepIndex == 1">{{info [stepIndex]}}</div>
+      <div class="puzzle" v-if="stepIndex == 6">
+        <table table table-bordered>
+          <tr><td> 1 </td><td> 2 </td><td> 3</td></tr>
+          <tr><td> 4 </td><td> 5 </td><td> 6 </td></tr>
+          <tr><td> 7 </td><td> 8 </td><td> 9 </td></tr>
+        </table>
+      </div>
       <div class="qcm" v-if="stepIndex == 3">
-        <ul class="list-unstyled">
-          <li>
-            <input type="checkbox" name="watteau" value="Watteau" />
-            <label for="watteau">Antoine Watteau</label>
-          </li>
-          <li>
-            <input type="checkbox" name="boucher" value="Boucher" />
-            <label for="boucher">François Boucher</label>
-          </li>
-          <li>
-            <input type="checkbox" name="fragonnard" value="Fragonnard" />
-            <label for="fragonnard">Jean-Honoré Fragonnard</label>
+        <ul id="v-for-object" class="list-unstyled">
+          <li v-for="item in painters" :key="item.name" >
+            <label v-bind:for="item.surname">
+            <input class="radiobtn" type="radio" v-bind:name="item.surname" v-bind:value="item.surname" v-model="checkedNames"/>
+            {{ item.name }} {{ item.surname }}
+            <span ></span>
+          </label>
           </li>
         </ul>
       </div>
       <div :class=classe[stepIndex] v-on:click="suite">
         <span class="suite-text" v-if="stepIndex == 1">Visualiser la carte</span>
-
       </div>
     </div>
 
@@ -37,42 +47,107 @@ export default {
   data () {
     return {
       stepIndex: 0,
+      error: false,
+      errorMsg: '',
+      errorNb: 0,
+      winMsg: [
+        '',
+        '',
+        '',
+        '',
+        'Et oui, il s\'agit de Watteau, ses contemporains Boucher et Fragonnard sont plus intéressés par les scènes de divinité comme Diane sortant du bain pour Boucher ou des scènes familiales comme le lecteur pour Fragonnard. Voici ton 1er indice: '
+      ],
+      indice: [
+        '',
+        '',
+        '',
+        '',
+        'Antoine Watteau'
+      ],
       title: [
         'Bienvenue',
         'Etape 1',
         'Carte',
-        'Etape 2'
+        'Etape 2',
+        '',
+        '',
+        'Etape 3'
       ],
       shortDescription: [
         'Es-tu prêt à découvrir une des plus  grandes affaire de vol du musée du Louvre ?',
         '',
         '',
-        '#Question à choix multiples'
+        '#Question à choix multiples',
+        '',
+        '',
+        'Puzzle'
       ],
       description: [
         'Il te faudra faire preuve d\'ingéniosité et bien observer les lieux pour résoudre les énigmes et découvrir le fin mot de l\'histoire.',
         'Rendez-vous à l\'aile Sully, c\'est par là que notre voleur est entré le 19 juin 1939 afin d\'accomplir son crime.',
         '',
-        'Nous sommes en plein coeur du style rococo, au milieu des années romantiques et des scènes idéalistes du 17e siecle. L\'artiste est passionné par les fêtes galantes, les costumes des dames et le théatre italien, en particulier la Commedia dell arte.'
+        'Nous sommes en plein coeur du style rococo, au milieu des années romantiques et des scènes idéalistes du 17e siecle. L\'artiste est passionné par les fêtes galantes, les costumes des dames et le théatre italien, en particulier la Commedia dell arte.',
+        '',
+        '',
+        'puzzle kdqskdqsd qsdqsdqsd qsd qsdqsdqsd q sqsdqsdqs qsd qsdq dqsd qdq s dqsdq '
       ],
       info: [
         'Si tu es d\'accord active la geoloc de ton smartphone et c\'est parti !',
         'Direction l\'aile Sully, 2ème étage, salle 36',
         '',
-        'De qui s\'agit-il ?'
+        'De qui s\'agit-il ?',
+        '',
+        '',
+        ''
       ],
       classe: [
         'fa fa-arrow-right',
         'fa fa-map-o',
+        'fa fa-arrow-right',
         'fa fa-arrow-right'
       ],
       stepAllow: [
         true
-      ]
+      ],
+      painters: [
+        {name: 'Antoine', surname: 'Watteau'},
+        {name: 'François', surname: 'Boucher'},
+        {name: 'Jean-Honoré', surname: 'Fragonnard'}
+      ],
+      checkedNames: []
     }
   },
   methods: {
+    close: function () {
+      if (this.error === true) {
+        this.stepIndex = this.stepIndex - 2
+        this.error = false
+      } else {
+        this.stepIndex++
+      }
+      alert(this.stepIndex)
+    },
     suite: function () {
+      if (this.stepIndex === 3) {
+        if (this.checkedNames !== 'Watteau' && this.errorNb === 2) {
+          /* Step 5 */
+          this.stepIndex = this.stepIndex + 3
+          this.error = true
+          this.errorNb = 0
+          this.errorMsg = 'Perdu. Passe à l\'étape suivante ! Le jeu continu, il y a encore plein d\'indice à découvrir '
+        } else if (this.checkedNames !== 'Watteau' && this.errorNb === 1) {
+          this.errorMsg = 'Non, ce n\'est pas ' + this.checkedNames + '. Attention, C\'est ta dernière chance !'
+          this.stepIndex = this.stepIndex + 1
+          this.error = true
+          this.errorNb++
+        } else if (this.checkedNames !== 'Watteau') {
+          this.stepIndex = this.stepIndex + 1
+          this.error = true
+          this.errorNb++
+          this.errorMsg = 'Non, ce n\'est pas ' + this.checkedNames + '. Retente ta chance !'
+          /* Step 4+1 */
+        }
+      }
       this.stepIndex = this.stepIndex + 1
     }
   }
@@ -89,12 +164,36 @@ h2 {
 .short-description .description {
   margin-top: 30vh;
 }
+.error-msg, .win-msg {
+  min-height: 40vh;
+  color: white;
+  font-weight: bold;
+  padding-top: 15vh;
+  padding-left: 5vh;
+  margin-right: 1vh;
+  background-color: #ff6600;
+  -webkit-box-shadow: 0px 8px 4px 0px #656565;
+  -moz-box-shadow: 0px 8px 4px 0px #656565;
+  filter:progid:DXImageTransform.Microsoft.dropshadow(OffX=0, OffY=8, Color='#656565', Positive='true');
+  zoom:1;
+  box-shadow: 0px 8px 4px 0px #656565;
+}
+.win-msg{
+  background-color: #206040;
+  color: white;
+  padding-right: 5vh;
+  padding-top: 5vh;
+}
 .description-bloc {
   font-size: 1.5rem;
   background-color: #CEFF33;
   min-height: 70vh;
   padding-top: 5vh;
   padding-left: 1vh;
+}
+.list-unstyled {
+  padding-left: 20vh;
+  padding-top: 1vh;
 }
 .fa {
   font-size: 3rem;
@@ -110,5 +209,13 @@ h2 {
 }
 .info {
   margin-top:5vh;
+}
+.radiobtn {
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    background-color: #eee;
+    border-radius: 50%;
 }
 </style>
