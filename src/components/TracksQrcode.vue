703a3cefb5fa8ab6c2acd49fbc0bf02f22aa4dd5
sandoka9 <chiara123456879@gmail.com>
2018-07-22 18:33:20 +0200

    Add qr code step

--- src/components/TracksQrcode.vue
+++ src/components/TracksQrcode.vue
@@ -5,7 +5,10 @@
       <div class="short-description">{{content.shortDescription}}</div>
       <div class="description">{{content.stepDescription}}</div>
       <div class="info">{{content.info}}</div>
-      <qrcode-reader @decode="onDecode"></qrcode-reader>
+      <QrcodeReader @decode="onDecode" @init="onInit">
+        <div class="decoded-content">{{ contentCode }}</div>
+        <LoadingIndicator v-show="loading" />
+      </QrcodeReader>
       <div>
         <input class="response form-control" type="text" name="response" v-bind:placeholder="content.info" v-model="response"/>
       </div>
@@ -15,7 +18,9 @@
 </template>
 
 <script>
-import VueQrcodeReader from 'vue-qrcode-reader'
+import 'vue-qrcode-reader/dist/vue-qrcode-reader.css'
+import { QrcodeReader } from 'vue-qrcode-reader'
+/* import InitHandler from '@/mixins/InitHandler' */
 /* https://pulilab.github.io/vue-qr-reader/#/quickstart */
 /* http://api.qrserver.com/v1/create-qr-code/?data=test!&size=100x100 */
 export default {
@@ -23,9 +28,11 @@ export default {
   props: {
     content: {}
   },
+  /* mixins: [ InitHandler ], */
   data () {
     return {
-      msg: 'Bienvenue sur la map'
+      msg: 'Bienvenue sur la map',
+      contentCode: ''
     }
   },
   /* eslint-disable */
@@ -35,7 +42,38 @@ export default {
   methods: {
     codeArrived (event) {
       console.log(event.detail[0])
+    },
+    onDecode (contentCode) {
+      this.contentCode = contentCode
+    },
+    async onInit (promise) {
+    // show loading indicator
+
+      try {
+        await promise
+
+        // successfully initialized
+      } catch (error) {
+        if (error.name === 'NotAllowedError') {
+          // user denied camera access permisson
+        } else if (error.name === 'NotFoundError') {
+          // no suitable camera device installed
+        } else if (error.name === 'NotSupportedError') {
+          // page is not served over HTTPS (or localhost)
+        } else if (error.name === 'NotReadableError') {
+          // maybe camera is already in use
+        } else if (error.name === 'OverconstrainedError') {
+          // passed constraints don't match any camera. Did you requested the front camera although there is none?
+        } else {
+          // browser is probably lacking features (WebRTC, Canvas)
+        }
+      } finally {
+        // hide loading indicator
+      }
     }
+  },
+  components: {
+    QrcodeReader
   },
   model: {
     event: 'change'
@@ -44,6 +82,19 @@ export default {
 </script>
 
 <style>
+
+.decoded-content {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  max-width: 100%;
+  padding: 0px 20px;
+  color: #fff;
+  font-weight: bold;
+  padding: 10px;
+  background-color: rgba(0,0,0,.5);
+}
 
 .radiobtn {
     top: 0;
