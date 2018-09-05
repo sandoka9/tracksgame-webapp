9015c48b141d7b8867d7563cee65d80fbd2b1028
sandoka9 <chiara123456879@gmail.com>
2018-09-06 00:13:56 +0200

    fetch json

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,6 +6,10 @@
       <router-link :to="{ name: 'DetailsGame', params: { gameId: 'sCmkwZ6L' } }"><img src="/index/images_game_sCmkwZ6L.jpg" /></router-link>
       <span class="content-game-title"> title </span>
     </div>
+    <div class="content-game2" v-for="item in content" :key="item.id">
+      <router-link :to="{ name: 'DetailsGame', params: { gameId: item.id } }"><img :src="item.src" /></router-link>
+      <span class="content-game-title"> {{item.title}} </span>
+    </div>
   </div>
 </template>
 
@@ -15,7 +19,23 @@ export default {
   name: 'Home',
   data () {
     return {
+      urlJson: 'http://localhost:8080/index/home.json',
+      content: {},
+      content2: {}
     }
+  },
+  beforeCreated () {
+    fetch('http://localhost:8080/index/home.json')
+      .then(function (response) {
+      // Do stuff with the response
+        // console.log('response' + JSON.stringify(response.json()))
+      })
+      .then(function (json) {
+        console.log('json' + JSON.stringify(json))
+      })
+      .catch(function (error) {
+        console.log('Looks like there was a problem: \n', error)
+      })
   },
   created () {
     localStorage.clear()
@@ -28,6 +48,52 @@ export default {
     localStorage.stepDone = JSON.stringify({})
     localStorage.stepDoneNb = 0
     localStorage.enigmaType = ''
+    console.log('top')
+    this.fetchData2()
+  },
+  methods: {
+    fetchData: function () {
+      console.log('this.urlJson' + this.urlJson)
+      fetch('http://localhost:8080/index/home.json')
+        .then(function (response) { return response.json() })
+      /* .then(r => r.json()) */
+        .then(function (json) {
+          /* console.log('json' + JSON.stringify(json)) */
+        })
+        .catch(function (e) { console.log('e: ' + e) })
+      /* for(var i = 0; i < json.products.length; i++) {
+        var listItem = document.createElement('li');
+        listItem.innerHTML = '<strong>' + json.products[i].Name + '</strong>';
+        listItem.innerHTML +=' can be found in ' + json.products[i].Location + '.';
+        listItem.innerHTML +=' Cost: <strong>£' + json.products[i].Price + '</strong>';
+        myList.appendChild(listItem);
+      } */
+
+      /*
+      fetch('products.json')
+      .then(function(response) { return response.json(); })
+      .then(function(json) {
+        for(var i = 0; i < json.products.length; i++) {
+          var listItem = document.createElement('li');
+          listItem.innerHTML = '<strong>' + json.products[i].Name + '</strong>';
+          listItem.innerHTML +=' can be found in ' + json.products[i].Location + '.';
+          listItem.innerHTML +=' Cost: <strong>£' + json.products[i].Price + '</strong>';
+          myList.appendChild(listItem);
+        }
+      })
+      */
+      /*
+        fetch('http://localhost:8080/index/home.json')
+          .then(function (response) { return response.json() })
+          .then(function (json) { console.log('json' + json.game.id) })
+          .catch(function (error) {
+            console.log('Looks like there was a problem: \n', error)
+          })
+      */
+    },
+    fetchData2: function () {
+
+    }
   }
 }
 
